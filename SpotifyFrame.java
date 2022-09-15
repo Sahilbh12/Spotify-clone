529bff3c33f4a7ff014e4aa8920d910e84dc71c0
Sahilbh12 <101415668+Sahilbh12@users.noreply.github.com>
2022-09-15 10:05:43 +0530

    Add files via upload

---
+++ SpotifyFrame.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/*You have to make seperate folder for audio and images files each and give its path in code for properly working of the app and 
+make sure you save all the java files in same folder and give its path properly for frame connection.*/
+
+public class SpotifyFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SpotifyFrame frame = new SpotifyFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SpotifyFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 816, 692);
+		setResizable(false);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel IntroImage = new JLabel("");
+		IntroImage.setIcon(new ImageIcon("C:\\CodeWithHarry\\JAVA course\\BasicJavaTutorial\\Spotify\\image\\Spotify_opening1.jpg"));
+		IntroImage.setBounds(0, 0, 800, 600);
+		contentPane.add(IntroImage);
+		
+		JButton enterBtn = new JButton("");
+		enterBtn.setBounds(0, 598, 800, 52);
+		enterBtn.setOpaque(false);
+		enterBtn.setBackground(new java.awt.Color(0,0,0,1));
+		enterBtn.setBorderPainted(false);
+		enterBtn.setBackground(new Color(230, 230, 250));
+		enterBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SpotifyFrame2 sptFrame = new SpotifyFrame2();
+				sptFrame.setVisible(true);
+			}
+		});
+		enterBtn.setIcon(new ImageIcon("C:\\CodeWithHarry\\JAVA course\\BasicJavaTutorial\\Spotify\\image\\spotify-button3.png"));
+		contentPane.add(enterBtn);
+
+	}
+
+}
